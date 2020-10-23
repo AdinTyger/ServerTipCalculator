91bf36cf416748ba699aa74a05b042bff5898d84
Logan <Loganstoddard5@gmail.com>
2020-10-22 21:34:02 -0400

    converted the oop code

--- servertips.py
+++ servertips.py
@@ -1,96 +1,110 @@
 import tkinter as tk
 
-def calcTakeHome(*args):
-    #setting values to what the user enter
-    Esubtotal = float(subtotal.get())
-    EnonCashTips = float(nonCashTips.get())
-    EcashTips = float(cashTips.get())
-    print(subtotal)
-    print(EnonCashTips)
-    print(EcashTips)
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.master.title("Take home tips")
+        #self.pack()
+        self.create_widgets() 
+    
+    def start(self):
+        pass
 
-    #calc vlaues to see what spilt of a night are
-    tipsD = float(EnonCashTips) + float(EcashTips)
-    busboyD = float(Esubtotal)*0.01
-    credittipsD = float(EnonCashTips)*0.04
-    if(userinput.get() == 'Habachi'):
-        chef = float(Esubtotal) * 0.07
-        chef = round(chef, 2)
-    else:
-        chef = 0
+    def calcTakeHome(self, *args):
+        #setting values to what the user enter
+        local_subtotal = float(self.subtotal.get())
+        local_nonCashTips = float(self.nonCashTips.get())
+        local_cashtips = float(self.cashTips.get())
+        print(local_subtotal)
+        print(local_nonCashTips)
+        print(local_cashtips)
 
-    minustipsD = credittipsD + busboyD + chef
-    totaltakeD = tipsD - minustipsD
-    print('total to bus boys is ' + str(busboyD))
-    totalToBusBoy.set(busboyD)
-    print('total to chefs is ' + str(chef))
-    totalToChef.set(chef)
-    print('total to the credit card fee is ' + str(credittipsD))
-    totalCreditCardFee.set(credittipsD)
-    print('total tips you get today are ' + str(totaltakeD))
-    totalTipsYouGet.set(totaltakeD)
-    print('total take out of tips ' + str(minustipsD))
-    totalTake.set(minustipsD)
+        #calc vlaues to see what spilt of a night are
+        tipsD = float(local_nonCashTips) + float(local_cashtips)
+        busboyD = float(local_subtotal)*0.01
+        credittipsD = float(local_nonCashTips)*0.04
+        if(self.userinput.get() == 'Habachi'):
+            chef = float(local_subtotal) * 0.07
+            chef = round(chef, 2)
+        else:
+            chef = 0
+
+        minustipsD = credittipsD + busboyD + chef
+        totaltakeD = tipsD - minustipsD
+        print('total to bus boys is ' + str(busboyD))
+        self.totalToBusBoy.set(busboyD)
+        print('total to chefs is ' + str(chef))
+        self.totalToChef.set(chef)
+        print('total to the credit card fee is ' + str(credittipsD))
+        self.totalCreditCardFee.set(credittipsD)
+        print('total tips you get today are ' + str(totaltakeD))
+        self.totalTipsYouGet.set(totaltakeD)
+        print('total take out of tips ' + str(minustipsD))
+        self.totalTake.set(minustipsD)
+
+    def create_widgets(self):
+        #Subtotal entry labels and feild
+        subtotalLabelQuestion = tk.Label(self.master, text='please type in your subtotal:')
+        subtotalLabelQuestion.grid(row=0, column=0)
+        self.subtotal = tk.StringVar()
+        subtotal_entry = tk.Entry(self.master, width=7, textvariable=self.subtotal)
+        subtotal_entry.grid(row=0, column=1)
+        subtotalLabel = tk.Label(self.master, text='$')
+        subtotalLabel.grid(row=0, column=2)
+
+        #nonCashTips entry labels and feild
+        nonCashTipsQuestion = tk.Label(self.master, text='please type in your tips excluding cash tips:')
+        nonCashTipsQuestion.grid(row=1, column=0)
+        self.nonCashTips = tk.StringVar()
+        nonCashTips_entry = tk.Entry(self.master, width=7, textvariable=self.nonCashTips)
+        nonCashTips_entry.grid(row=1, column=1)
+        nonCashTipsLabel = tk.Label(self.master, text='$')
+        nonCashTipsLabel.grid(row=1, column=2)
+
+        #cashTips entry labels and feild
+        cashTipsQuestion = tk.Label(self.master, text='please type in your cashtips:')
+        cashTipsQuestion.grid(row=2, column=0)
+        self.cashTips = tk.StringVar()
+        cashTips_entry = tk.Entry(self.master, width=7, textvariable=self.cashTips)
+        cashTips_entry.grid(row=2, column=1)
+        cashTipsLabel = tk.Label(self.master, text='$')
+        cashTipsLabel.grid(row=2, column=2)
+
+        #Subtotal entry labels and feild
+        userinputQuestion = tk.Label(self.master, text='Were you Habachi or Dine in')
+        userinputQuestion.grid(row=3, column=0)
+        self.userinput = tk.StringVar()
+        self.userinput.set("Dine in") # initial value
+        option = tk.OptionMenu(self.master, self.userinput, "Dine in", "Habachi")
+        option.grid(row=3, column=1)
+        
+
+        startButton = tk.Button(self.master, text="Calculate", command=self.calcTakeHome)
+        startButton.grid(column=0, row=4)
+
+        self.totalToBusBoy = tk.StringVar()
+        tk.Label(self.master, textvariable=self.totalToBusBoy).grid(column=1, row=5,)
+        tk.Label(self.master, text="Total to bus boy:").grid(column=0, row=5,)
+
+        self.totalToChef = tk.StringVar()
+        tk.Label(self.master, textvariable=self.totalToChef).grid(column=1, row=6,)
+        tk.Label(self.master, text="Total to Chef:").grid(column=0, row=6,)
+
+        self.totalCreditCardFee = tk.StringVar()
+        tk.Label(self.master, textvariable=self.totalCreditCardFee).grid(column=1, row=7,)
+        tk.Label(self.master, text="Credit Card Fee:").grid(column=0, row=7,)
+
+        self.totalTipsYouGet = tk.StringVar()
+        tk.Label(self.master, textvariable=self.totalTipsYouGet).grid(column=1, row=8,)
+        tk.Label(self.master, text="Tips you get:").grid(column=0, row=8,)
+
+        self.totalTake = tk.StringVar()
+        tk.Label(self.master, textvariable=self.totalTake).grid(column=1, row=9,)
+        tk.Label(self.master, text="Total Take Home:").grid(column=0, row=9,)
+
 
 root = tk.Tk()
-root.title("Take home tips")
+app = Application(master=root)
+app.mainloop()
 
-#Subtotal entry labels and feild
-subtotalLabelQuestion = tk.Label(root, text='please type in your subtotal:')
-subtotalLabelQuestion.grid(row=0, column=0)
-subtotal = tk.StringVar()
-subtotal_entry = tk.Entry(root, width=7, textvariable=subtotal)
-subtotal_entry.grid(row=0, column=1)
-subtotalLabel = tk.Label(root, text='$')
-subtotalLabel.grid(row=0, column=2)
-
-#Subtotal entry labels and feild
-nonCashTipsQuestion = tk.Label(root, text='please type in your tips excluding cash tips:')
-nonCashTipsQuestion.grid(row=1, column=0)
-nonCashTips = tk.StringVar()
-nonCashTips_entry = tk.Entry(root, width=7, textvariable=nonCashTips)
-nonCashTips_entry.grid(row=1, column=1)
-nonCashTipsLabel = tk.Label(root, text='$')
-nonCashTipsLabel.grid(row=1, column=2)
-
-#Subtotal entry labels and feild
-cashTipsQuestion = tk.Label(root, text='please type in your cashtips:')
-cashTipsQuestion.grid(row=2, column=0)
-cashTips = tk.StringVar()
-cashTips_entry = tk.Entry(root, width=7, textvariable=cashTips)
-cashTips_entry.grid(row=2, column=1)
-cashTipsLabel = tk.Label(root, text='$')
-cashTipsLabel.grid(row=2, column=2)
-
-#Subtotal entry labels and feild
-subtotalLabelQuestion = tk.Label(root, text='Were you Habachi or Dine in')
-subtotalLabelQuestion.grid(row=3, column=0)
-userinput = tk.StringVar()
-userinput.set("Dine in") # initial value
-option = tk.OptionMenu(root, userinput, "Dine in", "Habachi")
-option.grid(row=3, column=1)
-
-startButton = tk.Button(root, text="Calculate", command=calcTakeHome)
-startButton.grid(column=0, row=4)
-
-totalToBusBoy = tk.StringVar()
-tk.Label(root, textvariable=totalToBusBoy).grid(column=1, row=5,)
-tk.Label(root, text="Total to bus boy:").grid(column=0, row=5,)
-
-totalToChef = tk.StringVar()
-tk.Label(root, textvariable=totalToChef).grid(column=1, row=6,)
-tk.Label(root, text="Total to Chef:").grid(column=0, row=6,)
-
-totalCreditCardFee = tk.StringVar()
-tk.Label(root, textvariable=totalCreditCardFee).grid(column=1, row=7,)
-tk.Label(root, text="Credit Card Fee:").grid(column=0, row=7,)
-
-totalTipsYouGet = tk.StringVar()
-tk.Label(root, textvariable=totalTipsYouGet).grid(column=1, row=8,)
-tk.Label(root, text="Tips you get:").grid(column=0, row=8,)
-
-totalTake = tk.StringVar()
-tk.Label(root, textvariable=totalTake).grid(column=1, row=9,)
-tk.Label(root, text="Total Take Home:").grid(column=0, row=9,)
-
-root.mainloop()
