1c3f2eacedce5294c024a2c783ec944d4adf677a
Logan <Loganstoddard5@gmail.com>
2020-10-22 21:44:24 -0400

    added a frame var to create_widgets class to more dynamically change the container of the app

--- servertips.py
+++ servertips.py
@@ -4,9 +4,10 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.frame = tk.Frame(master)
         self.master.title("Take home tips")
-        #self.pack()
-        self.create_widgets() 
+        self.frame.pack()
+        self.create_widgets(self.frame) 
     
     def start(self):
         pass
@@ -43,65 +44,65 @@ class Application(tk.Frame):
         print('total take out of tips ' + str(minustipsD))
         self.totalTake.set(minustipsD)
 
-    def create_widgets(self):
+    def create_widgets(self, Frame):
         #Subtotal entry labels and feild
-        subtotalLabelQuestion = tk.Label(self.master, text='please type in your subtotal:')
+        subtotalLabelQuestion = tk.Label(Frame, text='please type in your subtotal:')
         subtotalLabelQuestion.grid(row=0, column=0)
         self.subtotal = tk.StringVar()
-        subtotal_entry = tk.Entry(self.master, width=7, textvariable=self.subtotal)
+        subtotal_entry = tk.Entry(Frame, width=7, textvariable=self.subtotal)
         subtotal_entry.grid(row=0, column=1)
-        subtotalLabel = tk.Label(self.master, text='$')
+        subtotalLabel = tk.Label(Frame, text='$')
         subtotalLabel.grid(row=0, column=2)
 
         #nonCashTips entry labels and feild
-        nonCashTipsQuestion = tk.Label(self.master, text='please type in your tips excluding cash tips:')
+        nonCashTipsQuestion = tk.Label(Frame, text='please type in your tips excluding cash tips:')
         nonCashTipsQuestion.grid(row=1, column=0)
         self.nonCashTips = tk.StringVar()
-        nonCashTips_entry = tk.Entry(self.master, width=7, textvariable=self.nonCashTips)
+        nonCashTips_entry = tk.Entry(Frame, width=7, textvariable=self.nonCashTips)
         nonCashTips_entry.grid(row=1, column=1)
-        nonCashTipsLabel = tk.Label(self.master, text='$')
+        nonCashTipsLabel = tk.Label(Frame, text='$')
         nonCashTipsLabel.grid(row=1, column=2)
 
         #cashTips entry labels and feild
-        cashTipsQuestion = tk.Label(self.master, text='please type in your cashtips:')
+        cashTipsQuestion = tk.Label(Frame, text='please type in your cashtips:')
         cashTipsQuestion.grid(row=2, column=0)
         self.cashTips = tk.StringVar()
-        cashTips_entry = tk.Entry(self.master, width=7, textvariable=self.cashTips)
+        cashTips_entry = tk.Entry(Frame, width=7, textvariable=self.cashTips)
         cashTips_entry.grid(row=2, column=1)
-        cashTipsLabel = tk.Label(self.master, text='$')
+        cashTipsLabel = tk.Label(Frame, text='$')
         cashTipsLabel.grid(row=2, column=2)
 
         #Subtotal entry labels and feild
-        userinputQuestion = tk.Label(self.master, text='Were you Habachi or Dine in')
+        userinputQuestion = tk.Label(Frame, text='Were you Habachi or Dine in')
         userinputQuestion.grid(row=3, column=0)
         self.userinput = tk.StringVar()
         self.userinput.set("Dine in") # initial value
-        option = tk.OptionMenu(self.master, self.userinput, "Dine in", "Habachi")
+        option = tk.OptionMenu(Frame, self.userinput, "Dine in", "Habachi")
         option.grid(row=3, column=1)
         
 
-        startButton = tk.Button(self.master, text="Calculate", command=self.calcTakeHome)
+        startButton = tk.Button(Frame, text="Calculate", command=self.calcTakeHome)
         startButton.grid(column=0, row=4)
 
         self.totalToBusBoy = tk.StringVar()
-        tk.Label(self.master, textvariable=self.totalToBusBoy).grid(column=1, row=5,)
-        tk.Label(self.master, text="Total to bus boy:").grid(column=0, row=5,)
+        tk.Label(Frame, textvariable=self.totalToBusBoy).grid(column=1, row=5,)
+        tk.Label(Frame, text="Total to bus boy:").grid(column=0, row=5,)
 
         self.totalToChef = tk.StringVar()
-        tk.Label(self.master, textvariable=self.totalToChef).grid(column=1, row=6,)
-        tk.Label(self.master, text="Total to Chef:").grid(column=0, row=6,)
+        tk.Label(Frame, textvariable=self.totalToChef).grid(column=1, row=6,)
+        tk.Label(Frame, text="Total to Chef:").grid(column=0, row=6,)
 
         self.totalCreditCardFee = tk.StringVar()
-        tk.Label(self.master, textvariable=self.totalCreditCardFee).grid(column=1, row=7,)
-        tk.Label(self.master, text="Credit Card Fee:").grid(column=0, row=7,)
+        tk.Label(Frame, textvariable=self.totalCreditCardFee).grid(column=1, row=7,)
+        tk.Label(Frame, text="Credit Card Fee:").grid(column=0, row=7,)
 
         self.totalTipsYouGet = tk.StringVar()
-        tk.Label(self.master, textvariable=self.totalTipsYouGet).grid(column=1, row=8,)
-        tk.Label(self.master, text="Tips you get:").grid(column=0, row=8,)
+        tk.Label(Frame, textvariable=self.totalTipsYouGet).grid(column=1, row=8,)
+        tk.Label(Frame, text="Tips you get:").grid(column=0, row=8,)
 
         self.totalTake = tk.StringVar()
-        tk.Label(self.master, textvariable=self.totalTake).grid(column=1, row=9,)
-        tk.Label(self.master, text="Total Take Home:").grid(column=0, row=9,)
+        tk.Label(Frame, textvariable=self.totalTake).grid(column=1, row=9,)
+        tk.Label(Frame, text="Total Take Home:").grid(column=0, row=9,)
 
 
 root = tk.Tk()
